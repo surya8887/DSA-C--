8b6348f56da41bc67a4ea9ee225f01d207a385e1
vijay Kumar <surya991837@gmail.com>
2025-06-21 07:47:04 +0530

    refactor linked list implementation and update print function

--- LinkedList/1link.cpp
+++ LinkedList/1link.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+//  Linked List
 class Node
 {
 public:
     int data;
     Node *next;
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+
     Node(int data)
     {
         this->data = data;
@@ -13,47 +19,40 @@ public:
     }
 };
 
-Node *insertatFirst(Node *head, int data)
-{
-    Node *newNode = new Node(data);
-    newNode->next = head;
-    head = newNode;
-    return head;
-}
-
-Node *insertAtLast(Node *head, int data)
-{
-    if (head == nullptr)
-    {
-        return new Node(data);
-    }
-    Node *current = head;
-    while (current->next != nullptr)
-    {
-        current = current->next;
-    }
-    current->next = new Node(data);
-    return head;
-}
-void printLinkedList(Node *head)
+void printList(Node *head)
 {
     while (head != nullptr)
     {
-        cout << head->data << " ";
+        cout << head->data << "->";
         head = head->next;
     }
-    cout << endl;
+    cout <<"Null"<<"\n";
+}
+Node*  ArrayTOList(vector<int> arr){
+    Node* head = new Node(arr[0]);
+    Node* curr = head;
+    for(int i =1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        curr->next = temp;
+        curr = temp;
+    }
+
+    return head;
 }
 int main()
 {
+    Node *head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    // printList(head);
 
-    Node *head = nullptr;
-    head = insertatFirst(head, 10);
-    head = insertatFirst(head, 20);
-    head = insertatFirst(head, 30);
-    head = insertAtLast(head, 40);
-    head = insertAtLast(head, 50);
-    head = insertAtLast(head, 60);
-    printLinkedList(head);
+    int* ptr = new int(10);
+    // cout <<*ptr<<endl;
+
+    // 
+    vector<int> arr = {1,2,3,4,5};
+    Node* head2 = ArrayTOList(arr);
+    printList(head2);
     return 0;
 }
