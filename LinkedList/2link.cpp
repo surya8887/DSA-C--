1e236b7151b4337f8b5da1a6702b3fb56899cf82
vijay Kumar <surya991837@gmail.com>
2025-06-21 12:06:05 +0530

    refactor linked list implementation and improve node management functions

--- LinkedList/2link.cpp
+++ LinkedList/2link.cpp
@@ -1,40 +1,100 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 class Node
 {
 public:
     int data;
     Node *next;
+
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+
     Node(int data)
     {
         this->data = data;
-        this->next = nullptr;
+        this->next = NULL;
     }
 };
 
-Node *CreateList(Node *head, vector<int> arr)
-{   Node *curr = head;
-    for (int i = 1; i < arr.size(); i++)
+//  create a linked list
+void createLinkedList(Node *&head, int data)
+{
+    Node *newNode = new Node(data);
+    if (head == NULL)
     {
-        head->next = new Node(arr[i]);
-        head = head->next;
+        head = newNode;
+        return;
     }
-    return curr;
+
+    Node *current = head;
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = newNode;
 }
+
+// Print Linked List
 void printList(Node *head)
 {
-    while (head != nullptr)
+    while (head != NULL)
     {
-        cout << head->data << " ";
+        cout << head->data << "->";
         head = head->next;
     }
-    cout<<endl;
+    cout << "NULL" << endl;
 }
+
+//  delete a node from head in the linked list
+Node* deleteNodeFromHead(Node* head)
+{
+    if (head == NULL)
+    {
+        return head;
+    }
+    Node* deleteNode = head;
+    head = head->next;
+    delete deleteNode;  // ← No parentheses here!
+    return head;
+}
+Node* AddAtHead(Node *head,int data){
+    Node* new_node = new Node(data);
+    new_node->next = head;
+    head = new_node;
+    return head;
+
+}
+
+
+Node * deleteNodeFromTail(Node* head){
+    if (head == NULL || head->next == NULL) return nullptr;
+    Node* curr = head ;
+    while (curr->next->next != NULL) {
+        curr = curr->next;
+    }
+    Node *deleteNode  = curr ->next;
+    curr -> next = nullptr ;  
+    delete deleteNode ;
+    return head ;
+} 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
-    Node *head = new Node(arr[0]);
-    head = CreateList(head, arr);
+    Node *head = new Node(0);
+
+    createLinkedList(head, 1);
+    createLinkedList(head, 2);
+    createLinkedList(head, 3);
+    createLinkedList(head, 4);
+    head = deleteNodeFromHead(head);
+    head = AddAtHead(head, 5);
     printList(head);
+    head = deleteNodeFromTail(head);
+    head = deleteNodeFromTail(head);
+    printList(head);
+
     return 0;
 }
